df87e3d399fd1a8326af3145b0c8a0f28701a86c
Ben McCormick <ben.mccormick@agworld.com.au>
2018-11-23 13:59:22 +0800

    Finishing up 04

--- pages/04/index.js
+++ pages/04/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Grid, Box, theme } from 'base';
+import { Grid, Box, theme, Text } from 'base';
 
 const PositionBox = styled(Box)`
   position: ${props => props.position};
@@ -67,30 +67,48 @@ RadialBox.defaultProps = {
   colour2: theme.colors.oranges[5],
 };
 
+const menuItems = ['Fashion', 'Jewellery', 'Watches', 'Fragrance'];
+
 const overflowStyles = { overflow: 'hidden', position: 'relative' };
 
-const Four = () => (
-  <Grid cover gridTemplateRows="repeat(10, 1fr)" gridTemplateColumns="repeat(auto-fill, calc(100vh / 10))">
-    <SplitGradientBox cover gridArea="1 / 2 / span 6 / span 6" left="-50%" transform="rotate(90deg)" colour1="rgba(234, 176, 162)" colour2="rgba(44, 65, 84)" />
-    
-    <RadialBox cover gridArea="1 / 1 / span 4 / span 4" colour1="rgba(226, 226, 226)" colour2="rgba(199, 225, 212)"/>
+const Four = () => {
+  const menu = menuItems.map(m => (
+    <Text key={m} color="grayscale.4">{m}</Text>
+  ));
 
-    <Box round cover gridArea="5 / 1 / span 4 / span 4" bg="rgba(207, 208, 177)" />
+  return (
+    <Grid cover gridTemplateRows="repeat(10, 1fr)" gridTemplateColumns="repeat(auto-fill, calc(100vh / 10))">
+      <SplitGradientBox cover gridArea="1 / 2 / span 6 / span 6" left="-50%" transform="rotate(90deg)" colour1="rgba(234, 176, 162)" colour2="rgba(44, 65, 84)" />
+      
+      <RadialBox cover gridArea="1 / 1 / span 4 / span 4" colour1="rgba(226, 226, 226)" colour2="rgba(199, 225, 212)"/>
+  
+      <Box round cover gridArea="5 / 1 / span 4 / span 4" bg="rgba(207, 208, 177)" />
+  
+      <Box cover gridArea="4 / 5 / span 6 / span 6" style={overflowStyles}>
+        <PositionBox round cover position="absolute" left="-50%" bg="rgba(234, 176, 162)" />
+      </Box>
+      <SplitGradientBox cover gridArea="5 / 5 / span 4 / span 4" left="-50%" colour1="rgba(234, 176, 162)" colour2="rgba(44, 65, 84)" />
+      
+      <Box cover gridArea="4 / 5 / span 6 / span 6" style={overflowStyles}>
+        <PositionBox round cover position="absolute" left="50%" bg="rgba(44, 65, 84)" />
+      </Box>
+      <SplitGradientBox cover gridArea="5 / 7 / span 4 / span 4" left="50%" colour1="rgba(234, 176, 162)" colour2="rgba(199, 225, 212)" />
+  
+      <RadialBox cover gridArea="3 / 5 / span 4 / span 4" transform="rotate(180deg)" colour1="rgba(207, 208, 177)" colour2="rgba(151, 196, 173)" />
+  
+      <RadialBox cover gridArea="7 / 7 / span 4 / span 4" colour1="rgba(151, 196, 173)" colour2="rgba(199, 225, 212)" />
+  
+      <Box as="nav" gridArea="1 / 12 / span 2 / span 5" justifyContent="space-between">
+        {menu}
+      </Box>
+  
+      <Text gridArea="6 / 12 / span 5 / span 6" fontSize="42px">
+        To be the Ultimate House of Luxury, defining style and creating desire, now and forever.
+      </Text>
+    </Grid>
+  );
+}
 
-    <Box cover gridArea="4 / 5 / span 6 / span 6" style={overflowStyles}>
-      <PositionBox round cover position="absolute" left="-50%" bg="rgba(234, 176, 162)" />
-    </Box>
-    <SplitGradientBox cover gridArea="5 / 5 / span 4 / span 4" left="-50%" colour1="rgba(234, 176, 162)" colour2="rgba(44, 65, 84)" />
-    
-    <Box cover gridArea="4 / 5 / span 6 / span 6" style={overflowStyles}>
-      <PositionBox round cover position="absolute" left="50%" bg="rgba(44, 65, 84)" />
-    </Box>
-    <SplitGradientBox cover gridArea="5 / 7 / span 4 / span 4" left="50%" colour1="rgba(234, 176, 162)" colour2="rgba(199, 225, 212)" />
 
-    <RadialBox cover gridArea="3 / 5 / span 4 / span 4" transform="rotate(180deg)" colour1="rgba(207, 208, 177)" colour2="rgba(151, 196, 173)" />
-
-    <RadialBox cover gridArea="7 / 7 / span 4 / span 4" colour1="rgba(151, 196, 173)" colour2="rgba(199, 225, 212)" />
-  </Grid>
-);
 
 export default Four;

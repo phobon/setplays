fcf897e5ef456099543cb7b506e1406ed99a9980
phobon <exponen7@gmail.com>
2019-06-22 19:57:45 +0800

    Fixing up some colours

--- pages/06/index.js
+++ pages/06/index.js
@@ -13,7 +13,7 @@ const FilterBox = styled(Box)`
     content: '';
     width: 100%;
     height: 100%;
-    box-shadow: 0 15px 25px hsla(33, 76%, 84%, 1), 0 5px 10px hsla(33, 76%, 84%, 1);
+    box-shadow: 0 15px 25px rgba(251, 174, 0, 0.7), 0 5px 10px rgba(251, 174, 0, 1);
     pointer-events: none;
     border-radius: inherit;
     opacity: ${props => props.active ? 0.5 : 0};
@@ -43,7 +43,7 @@ Filter.defaultProps = {
 };
 
 const Six = () => (
-  <Box fullWidth fullHeight>
+  <Box fullWidth fullHeight bg="#055CF9">
     <Card flexDirection="column" width={375} height={812} justifyContent="flex-start" borderRadius="30px" pt={7} pb={3} bg="background" alignItems="flex-start">
       <Box fullWidth justifyContent="space-between" px={5} mb={6}>
       <Vector width="16" height="10" viewBox="0 0 16 10">
@@ -53,18 +53,18 @@ const Six = () => (
         <Image width={32} height={32} borderRadius={4} src="https://source.unsplash.com/random/32x32" />
       </Box>
 
-      <Text ml={5} fontWeight="bold" fontSize={5} color="purples.1">Feed</Text>
+      <Text ml={5} fontWeight="bold" fontSize={5} color="#3C3A5D">Feed</Text>
       <Text ml={5} color="grayscale.5" mb={6}>Set ut amnet dolor</Text>
 
       <Grid fullWidth gridTemplateColumns="repeat(3, 1fr)" gridGap={2} px={5} mb={6}>
-        <Filter bg="oranges.7" color="white" count={24} label="photos" active />
+        <Filter bg="#FEAC00" color="white" count={24} label="photos" active />
         <Filter color="reds.7" count={48} label="articles" />
         <Filter color="purples.7" count="22k" label="followers" />
       </Grid>
 
       <Box fullWidth css={{ overflow: 'hidden', position: "relative" }} height={26} mb={5}>
         <Box width="104%" css={{ position: 'absolute', left: 32 }} justifyContent="space-between">
-          <Box px={2} py={1} bg="oranges.9" color="oranges.2" borderRadius={4}>
+          <Box px={2} py={1} bg="#FFE6C2" color="oranges.2" borderRadius={4}>
             <Text color="inherit" fontSize={0} textStyle="caps" mx={1}>chat</Text>
           </Box>
           <Box px={2} py={1}>
@@ -79,7 +79,7 @@ const Six = () => (
         </Box>
       </Box>
 
-      <Text ml={5} fontWeight="bold" fontSize={2} mb={2} color="purples.1">My photos</Text>
+      <Text ml={5} fontWeight="bold" fontSize={2} mb={2} color="#3C3A5D">My photos</Text>
       <Box fullWidth css={{ overflow: 'hidden', position: "relative" }} height={140} alignItems="flex-start">
         <Box css={{ position: 'absolute', left: 32 }}>
           <Box bg="grayscale.8" borderRadius={5} height={96} width={96} mr={2}>
@@ -97,7 +97,7 @@ const Six = () => (
         </Box>
       </Box>
 
-      <Text ml={5} fontWeight="bold" fontSize={2} mb={2} color="purples.1">My videos</Text>
+      <Text ml={5} fontWeight="bold" fontSize={2} mb={2} color="#3C3A5D">My videos</Text>
       <Box fullWidth css={{ overflow: 'hidden', position: "relative" }} height={112} mb={5}>
         <Box css={{ position: 'absolute', left: 32 }}>
           <Image borderRadius={5} height={112} width={192} mr={2} src="https://source.unsplash.com/random/192x112" />
